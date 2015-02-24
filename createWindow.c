28ced5e7c70c00554b52821a547a1e45b99475ad
Carlos Rodriguez <juan.carlos.rodriguez.rodriguez@intel.com>
2015-02-24 06:02:31 -0600

    improved window, 2 buttons

--- createWindow.c
+++ createWindow.c
@@ -1,9 +1,9 @@
 #include <gtk/gtk.h>
 
-//Callback function
-static void hello( GtkWidget *widget, gpointer data )
+//Callback functionin improved (with data)
+static void callback( GtkWidget *widget, gpointer data )
 {
-	g_print ("Hello World \n");
+	g_print ("Hello - %s was pressed  \n", (gchar *) data);
 }
 
 
@@ -13,15 +13,8 @@ static gboolean delete_event( GtkWidget *widget,
 							 GdkEvent *event,
 							 gpointer data )
 {
-	g_print ("Hello World \n");
-
-	return TRUE;
-}
-
-static void destroy( GtkWidget *widget,
-					 gpointer data )
-{
 	gtk_main_quit();
+	return FALSE;
 }
 
 int main ( int argc, char *argv[] )
@@ -29,6 +22,7 @@ int main ( int argc, char *argv[] )
 	//storage type for widgets
 	GtkWidget *window;
 	GtkWidget *button;
+	GtkWidget *box1;
 
 	//arguments are prsed and returned to the app
 	gtk_init (&argc, &argv);
@@ -36,34 +30,49 @@ int main ( int argc, char *argv[] )
 	//create new window
 	window = gtk_window_new( GTK_WINDOW_TOPLEVEL);
 
+	//This just sets the title
+	gtk_window_set_title (GTK_WINDOW (window), "Titulo (=");
+
 	/* Trigered by a "delete-event" given by the window manager (close option),
 	 * in this case we call the delete_event () function as defined.
 	 * The data passed is NULL and is ignored in the function */
 	g_signal_connect (window, "delete-event", G_CALLBACK (delete_event), NULL);
 
-	/* "destroy event -> signal handler, occurs when we call "gtk_widget_destroy()
-	 * on the window, or if the "delete-event" callback returns false */
-	g_signal_connect (window, "destroy", G_CALLBACK (destroy), NULL);
-
 	//sets border width
-	gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+	gtk_container_set_border_width (GTK_CONTAINER (window), 15);
+
+	//Create a box to "pack" widgets
+	box1 = gtk_hbox_new (FALSE, 0);
+
+	//put the box into the main window
+	gtk_container_add (GTK_CONTAINER (window), box1);
 
 	//Creates button with label "Hello World", and allocates in memory
-	button = gtk_button_new_with_label ("Hello World");
+	button = gtk_button_new_with_label ("Button 1");
 
 	//When button receives "clicked" signal, calls hello()
-	g_signal_connect (button, "clicked", G_CALLBACK (hello), NULL);
+	g_signal_connect (button, "clicked", G_CALLBACK (callback), "button 1");
 
-	//This will cause the window to be destroyed by calling gtk_widget_destroy(window)
-	g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+	//Pack this button into the invisible box
+	gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
 
-	//packs the button into the window (a gtk container)
-	gtk_container_add (GTK_CONTAINER (window), button);
-
-	//display the new widget
+	//This tells GTK that the button is ready to be showed (=
 	gtk_widget_show (button);
 
-	//display the window
+	//Creates button with label "Hello World", and allocates in memory
+	button = gtk_button_new_with_label ("Button 2");
+
+	//When button receives "clicked" signal, calls hello()
+	g_signal_connect (button, "clicked", G_CALLBACK (callback), "button 2");
+
+	//Pack this button into the invisible box
+	gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
+
+	//Showing all the stuff...
+	gtk_widget_show (button);
+
+	gtk_widget_show (box1);
+
 	gtk_widget_show (window);
 
 	/* GTK aplicatiosn must have gtk_main. Things ends up in here and waits
